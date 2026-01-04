b0507d597a03b69d0feb6986dbc9e1cd5675fbf2
Lubabalo18 <Lubabalomagade9@gmail.com>
2026-01-04 16:27:42 +0200

    Add files via upload

---
+++ git_task/git_task/Hello_world.py
@@ -0,0 +1,11 @@
+print("Hello, World!")
+# git status  
+# git add Hello_world.py
+# git commit -m "Initial commit: Added Hello_world.py"
+# git push origin main
+
+# git add hello_world.py
+# git commit -m "Update greeting to 'Git is awesome!'"
+print("Git is awesome!")
+# git status 
+#git log --oneline
